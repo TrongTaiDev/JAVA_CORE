96adebc4653409e0a4a18b21d12246c99862228e
trongtaidev <trongtai.dev@gmail.com>
2017-09-23 22:40:58 +0700

    Cập nhật bài 3

---
+++ ReviewQuestions_Chapter1/src/Bai3/StringEquals.java
@@ -0,0 +1,11 @@
+package Bai3;
+
+public class StringEquals {
+	public static void main(String argn[]) {
+		String s1 = "Vietnam";
+		String s2 = new String(s1);
+		
+		if(s1 == s2) System.out.println("s1 == s2");
+		if(s1.equals(s2)) System.out.println("s1 equals s2");		
+	}
+}
