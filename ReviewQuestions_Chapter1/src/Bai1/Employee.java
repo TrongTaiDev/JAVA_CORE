a528d9ad2b94b8d54881fde9bcb14686c8f23f33
trongtaidev <trongtai.dev@gmail.com>
2017-09-23 22:07:26 +0700

    ok

---
+++ ReviewQuestions_Chapter1/src/Bai1/Employee.java
@@ -0,0 +1,31 @@
+package Bai1;
+
+public class Employee {
+	public int employeeId;
+	public String firstName, lastName;
+	public int yearStarted;
+	
+	@Override
+	public int hashCode() {
+		return employeeId;
+	}
+	
+	//Overload
+	public boolean equals(Employee obj) {
+		return this.employeeId == obj.employeeId;
+	}
+	
+	public static void main(String argn[]) {
+		Employee one = new Employee();
+		one.employeeId = 101;
+		
+		Employee two = new Employee();
+		two.employeeId = 101;
+		
+		if(one.equals(two)) {
+			System.out.println("Giống!");
+		}else {
+			System.out.println("Khác!");
+		}
+	}
+}
